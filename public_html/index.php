de146e2526091a951e68757228270f418bd4740c
BrunoButzke <brunobutzkesouza@gmail.com>
2018-02-28 17:05:21 -0300

    First commit

---
+++ public_html/index.php
@@ -0,0 +1,36 @@
+<head>
+	<title>Tela de Login</title>
+
+	<link rel="stylesheet" type="text/css" href="style2.css">
+	<script type="text/javascript" src="script2.js"></script>
+
+
+</head>
+<body>
+	<div class="container">
+		<form action="menu.php" method="POST">
+			<fieldset class="login">
+
+				<p>Bem-Vindo</p><br><br>
+
+				<label>Login</label><br><input type="text" ><br><br>
+				<label>Senha</label><br><input type="password"><br><br>
+
+				<button class="botoes">ENTRAR</button> <br>
+
+				<a href="#" title="Esqueci minha senha !">
+					<img src="imagens/Esqueceu_senha.png" height="20" width="20">
+				</a>
+			</fieldset>
+		</form>
+
+
+	</div>
+</body>
+
+
+
+
+
+
+
